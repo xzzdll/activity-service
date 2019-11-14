c64ee627cce739829fa008caba23c6a93f34007a
John <john.zhu@binance.com>
2019-11-14 14:12:00 +0800

    delete useless

--- models/sayModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const saySchema = new Schema({
-  date: { type: String, required: true },
-  content: { type: String, required: true },
-})
-
-module.exports.say = mongoose.model('say', saySchema)
--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const userSchema = new Schema({
-  username: { type: String },
-  password: { type: String, required: true },
-})
-
-module.exports.account = mongoose.model('user', userSchema)
